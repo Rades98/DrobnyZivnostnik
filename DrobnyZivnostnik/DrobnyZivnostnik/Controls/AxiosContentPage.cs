59415a418e10bf6ab5c7857a775138ae0690e27c
Rades98 <37796889+Rades98@users.noreply.github.com>
2021-04-08 11:02:53 +0200

    added doc

--- DrobnyZivnostnik/DrobnyZivnostnik/Controls/AxiosContentPage.cs
+++ DrobnyZivnostnik/DrobnyZivnostnik/Controls/AxiosContentPage.cs
@@ -9,6 +9,16 @@
     /// <seealso cref="Xamarin.Forms.ContentPage" />
     public class AxiosContentPage : ContentPage
     {
+
+        /// <summary>
+        /// Application developers can override this method to provide behavior when the back button is pressed.
+        /// </summary>
+        /// <returns>
+        /// To be added.
+        /// </returns>
+        /// <remarks>
+        /// To be added.
+        /// </remarks>
         protected override bool OnBackButtonPressed()
         {
             if (!(Application.Current.MainPage is MasterDetailPage mainPage))
