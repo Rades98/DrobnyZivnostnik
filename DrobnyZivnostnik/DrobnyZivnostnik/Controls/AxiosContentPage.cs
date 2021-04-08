2cf80233f5e3b69de93bcbfa9e955ca780bdd375
Rades98 <37796889+Rades98@users.noreply.github.com>
2021-04-08 11:15:30 +0200

    blah

--- DrobnyZivnostnik/DrobnyZivnostnik/Controls/AxiosContentPage.cs
+++ DrobnyZivnostnik/DrobnyZivnostnik/Controls/AxiosContentPage.cs
@@ -10,15 +10,6 @@
     public class AxiosContentPage : ContentPage
     {
 
-        /// <summary>
-        /// Application developers can override this method to provide behavior when the back button is pressed.
-        /// </summary>
-        /// <returns>
-        /// To be added.
-        /// </returns>
-        /// <remarks>
-        /// To be added.
-        /// </remarks>
         protected override bool OnBackButtonPressed()
         {
             if (!(Application.Current.MainPage is MasterDetailPage mainPage))
