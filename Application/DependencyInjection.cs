a70aee1a75f87874ea98dd8b9cc7f33209ea7ebc
Rades98 <37796889+Rades98@users.noreply.github.com>
2021-04-25 19:56:48 +0200

    deleted useless folders

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,13 +1,7 @@
 ﻿namespace Application
 {
-    using AutoMapper;
     using Database;
-    using Domain.Entities;
     using Microsoft.Extensions.DependencyInjection;
-    using Models.Address;
-    using Models.TravelOrder;
-    using Models.User;
-    using Models.Vehicle;
     using Services;
     using Services.Interfaces;
     using Xamarin.Forms;
@@ -36,20 +30,7 @@
             DependencyService.Register<IVehicleService, VehicleService>();
             DependencyService.Register<ITravelOrderService, TravelOrderService>();
 
-            DependencyService.RegisterSingleton(new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<Address, AddressModel>();
-                cfg.CreateMap<AddressModel, Address>();
-
-                cfg.CreateMap<TravelOrderFront, TravelOrderFrontModel>();
-                cfg.CreateMap<TravelOrderFrontModel, TravelOrderFront>();
-
-                cfg.CreateMap<User, UserModel>();
-                cfg.CreateMap<UserModel, User>();
-
-                cfg.CreateMap<Vehicle, VehicleModel>();
-                cfg.CreateMap<VehicleModel, Vehicle>();
-            }).CreateMapper());
+            DependencyService.RegisterSingleton(new MappingProfiler());
         }
     }
 }
